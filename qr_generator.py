eb394948e193805eb532fa942d839bc81aa5f9e0
Rafael Torres Escobar <torresc.rafael@gmail.com>
2022-10-12 12:54:44 -0500

    change url

--- qr_generator.py
+++ qr_generator.py
@@ -6,12 +6,14 @@ from qrcode import QRCode
 
 load_dotenv()
 
-url = os.getenv('MY_URL')
-
-print(url)
+# url = os.getenv('MY_URL')
+text = os.getenv('MY_TEXT')
+email = os.getenv('SEND_EMAIL')
 qr = QRCode(version=1, box_size=10, border=5)
 
-qr.add_data(url)
+qr.add_data(email)
+# qr.add_data('\n\n')
+# qr.add_data(os.getenv('LINK2'))
 qr.make(fit=True)
 
 img = qr.make_image(fill='black', back_color='white')
